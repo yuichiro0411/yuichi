18fbc9765419ad8d86e19085d9e091ea27cfc0a6
Yuichiro.Takiyama <yuichiro0411@me.com>
2018-11-20 22:05:48 +0900

    Just test.

---
+++ app/controllers/tells_controller.rb
@@ -0,0 +1,6 @@
+class TellsController < ApplicationController
+	def index
+	end
+	def test
+	end
+end
---
+++ test/controllers/tells_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TellsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
